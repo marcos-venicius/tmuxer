a255d2199d83d5137d1b63181c0f4194da5e4ff1
Marcos Venicius <marcosdevelopment.business@gmail.com>
2026-02-19 08:55:17 -0300

    bake project version from .nimble file

--- src/tmuxer.nim
+++ src/tmuxer.nim
@@ -6,17 +6,21 @@ import arggetter
 import configgetter
 import actions
 
+# Comes directly from the build
+const NimblePkgVersion {.strdefine.} = "0.0.0"
+
 let programName = shift().get()
 var action: Option[string] = none(string)
 
 proc showHelp(withError: bool) =
   stderr.writeLine(&"Usage: {programName} [options] <action>")
   stderr.writeLine("Actions:")
-  stderr.writeLine("  up          - Start tmux sessions as defined in the config file")
-  stderr.writeLine("  down        - Stop tmux sessions as defined in the config file")
-  stderr.writeLine("  view        - Print the parsed configuration to the console")
+  stderr.writeLine("  up              - Start tmux sessions as defined in the config file")
+  stderr.writeLine("  down            - Stop tmux sessions as defined in the config file")
+  stderr.writeLine("  view            - Print the parsed configuration to the console")
   stderr.writeLine("Options:")
-  stderr.writeLine("  -h, --help  - Show this help message")
+  stderr.writeLine("  -h, --help      - Show this help message")
+  stderr.writeLine("  -v, --version   - Show current app version")
   quit(if withError: 1 else: 0)
 
 while true:
@@ -30,6 +34,9 @@ while true:
   case value:
     of "--help", "-h":
       showHelp(false)
+    of "--version", "-v":
+      echo NimblePkgVersion
+      quit(0)
     of "up", "down", "view":
       if action.isSome:
         stderr.writeLine("multiple actions specified. only one action can be performed at a time")
--- tmuxer.nimble
+++ tmuxer.nimble
@@ -1,4 +1,4 @@
-version     = "0.1.0"
+version     = "1.0.0"
 author      = "Marcos"
 description = "A tmux setup tool"
 license     = "MIT"
